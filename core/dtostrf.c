b7535d877acbf7b3b45ffa497f7aad9e4f889e4d
Meco Man <920369182@qq.com>
2021-12-19 22:21:21 -0500

    使用rt_sprintf代替sprintf

--- core/dtostrf.c
+++ core/dtostrf.c
@@ -23,11 +23,12 @@ http://www.nongnu.org/avr-libc/user-manual/group__avr__stdlib.html#ga060c998e77f
 */
 
 #include "dtostrf.h"
+#include <rtthread.h>
 
 char *dtostrf(double val, signed char width, unsigned char prec, char *sout)
 {
     char fmt[20];
-    sprintf(fmt, "%%%d.%df", width, prec);
-    sprintf(sout, fmt, val);
+    rt_sprintf(fmt, "%%%d.%df", width, prec);
+    rt_sprintf(sout, fmt, val);
     return sout;
 }
