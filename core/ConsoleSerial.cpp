b4b12aa7c01d71a659bf537e62657072cb5fcee3
Meco Man <920369182@qq.com>
2022-02-11 21:05:12 -0500

    调整注释

--- core/ConsoleSerial.cpp
+++ core/ConsoleSerial.cpp
@@ -18,20 +18,20 @@ ConsoleSerial::ConsoleSerial(void)
 
 void ConsoleSerial::begin(uint32_t BaudRate)
 {
-
+    /* Nothing need to do */
 }
 
+/*Code to display letter when given the ASCII code for it*/
 size_t ConsoleSerial::write(uint8_t c)
 {
-    /*Code to display letter when given the ASCII code for it*/
     rt_kprintf("%c", c);
     return 1;
 }
 
+/*Code to display array of chars when given a pointer to the beginning of the array and a size
+    -- this will not end with the null character*/
 size_t ConsoleSerial::write(const uint8_t *buffer, size_t size)
 {
-    /*Code to display array of chars when given a pointer to the beginning of the array and a size
-        -- this will not end with the null character*/
     return rt_device_write(rt_console_get_device(), 0, buffer, size);
 }
 
