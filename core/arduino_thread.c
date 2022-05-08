5b112381841ed28ddd2e0078c675315f21399572
Man, Jianting (Meco) <920369182@qq.com>
2022-05-07 23:20:55 -0400

    support hwtimer device driver (#10)
    * support hwtimer device driver
    * 实现pulseIn、pulseInLong函数，已经在超声波模块上测试通过，这两个函数也可以应付按键等复杂抖动下的输入捕获

--- core/arduino_thread.c
+++ core/arduino_thread.c
@@ -9,7 +9,7 @@
  */
 
 #include <Arduino.h>
-#include <rtthread.h>
+#include <rtdevice.h>
 
 #define DBG_TAG    "Arduino"
 #define DBG_LVL    DBG_INFO
@@ -23,13 +23,45 @@
 #define RTDUINO_THREAD_PRIO     30
 #endif /* RTDUINO_THREAD_PRIO */
 
+#ifndef RTDUINO_DEFAULT_HWTIMER_DEVICE_NAME
+#define RTDUINO_DEFAULT_HWTIMER_DEVICE_NAME "timer0" /* dummy name */
+#endif /* RTDUINO_DEFAULT_HWTIMER_DEVICE_NAME */
+
 /* initialization for BSP; maybe a blank function  */
 RT_WEAK void initVariant(void)
 {
 }
 
+static rt_err_t hwtimer_timeout_cb(rt_device_t dev, rt_size_t size)
+{
+
+    return RT_EOK;
+}
+
+static void hwtimer_init(void)
+{
+    rt_device_t hwtimer_device;
+    rt_hwtimer_mode_t mode = HWTIMER_MODE_PERIOD;
+    rt_uint32_t freq = 1000000; /* 1us */
+    rt_hwtimerval_t val;
+
+    val.sec = 1;
+    val.usec = 0;
+
+    hwtimer_device = rt_device_find(RTDUINO_DEFAULT_HWTIMER_DEVICE_NAME);
+    if(hwtimer_device != RT_NULL)
+    {
+        rt_device_open(hwtimer_device, RT_DEVICE_OFLAG_RDWR);
+        rt_device_set_rx_indicate(hwtimer_device, hwtimer_timeout_cb);
+        rt_device_control(hwtimer_device, HWTIMER_CTRL_FREQ_SET, &freq);
+        rt_device_control(hwtimer_device, HWTIMER_CTRL_MODE_SET, &mode);
+        rt_device_write(hwtimer_device, 0, &val, sizeof(val));
+    }
+}
+
 static void arduino_entry(void *parameter)
 {
+    hwtimer_init();
     initVariant();
     setup();
 
--- core/wiring_pulse.c
+++ core/wiring_pulse.c
@@ -15,6 +15,49 @@
 #define DBG_LVL    DBG_INFO
 #include <rtdbg.h>
 
+static struct rt_semaphore pulsein_sem;
+static rt_bool_t pulsein_sem_init_flag = RT_FALSE;
+
+struct
+{
+    rt_bool_t first_pulse_coming;
+    rt_hwtimerval_t first_pulse_timestamp;
+    rt_bool_t second_pulse_coming;
+    rt_hwtimerval_t second_pulse_timestamp;
+    rt_uint8_t state;
+    rt_int32_t rt_isr_pin;
+    rt_device_t hwdevice;
+}pulse_record;
+
+static void pulsein_pin_interrupt_cb(void *args)
+{
+    rt_hwtimerval_t timestamp;
+
+    rt_device_read(pulse_record.hwdevice, 0, &timestamp, sizeof(timestamp));
+
+    if(pulse_record.first_pulse_coming == RT_FALSE
+        && pulse_record.second_pulse_coming == RT_FALSE
+        && rt_pin_read(pulse_record.rt_isr_pin) == pulse_record.state)
+    {
+        pulse_record.first_pulse_timestamp.sec = timestamp.sec;
+        pulse_record.first_pulse_timestamp.usec = timestamp.usec;
+        pulse_record.first_pulse_coming = RT_TRUE;
+    }
+    else if(pulse_record.first_pulse_coming == RT_TRUE
+        && pulse_record.second_pulse_coming == RT_FALSE
+        && rt_pin_read(pulse_record.rt_isr_pin) != pulse_record.state)
+    {
+        pulse_record.second_pulse_timestamp.sec = timestamp.sec;
+        pulse_record.second_pulse_timestamp.usec = timestamp.usec;
+        pulse_record.second_pulse_coming = RT_TRUE;
+        rt_sem_release(&pulsein_sem); /* has waitted the second pulse */
+    }
+    else
+    {
+        /* logic error, skip */
+    }
+}
+
 /* Measures the length (in microseconds) of a pulse on the pin; state is HIGH
  * or LOW, the type of pulse to measure.  Works on pulses from 2-3 microseconds
  * to 3 minutes in length, but must be called at least a few dozen microseconds
@@ -24,8 +67,53 @@
  */
 unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout)
 {
-    LOG_E("pulseIn() has not been implemented yet!");
-    return 0;
+    rt_int32_t rt_pin;
+    rt_err_t rt_err;
+    long delta;
+
+    RT_ASSERT(state == HIGH || state == LOW);
+
+    pulse_record.hwdevice = rt_device_find(RTDUINO_DEFAULT_HWTIMER_DEVICE_NAME);
+    if(pulse_record.hwdevice == RT_NULL)
+    {
+        LOG_E("Cannot find hardware timer!");
+        return 0;
+    }
+
+    if(pulsein_sem_init_flag == RT_FALSE)
+    {
+        rt_sem_init(&pulsein_sem, "pulsein", 0, RT_IPC_FLAG_PRIO);
+        pulsein_sem_init_flag = RT_TRUE;
+    }
+
+    rt_pin = pin_map_table[pin].rt_pin;
+
+    /* clear parameters */
+    pulsein_sem.value = 0; /* clear sem value */
+    pulse_record.first_pulse_coming = RT_FALSE;
+    pulse_record.first_pulse_timestamp.sec = 0;
+    pulse_record.first_pulse_timestamp.usec = 0;
+    pulse_record.second_pulse_coming = RT_FALSE;
+    pulse_record.second_pulse_timestamp.sec = 0;
+    pulse_record.second_pulse_timestamp.usec = 0;
+    pulse_record.state = state;
+    pulse_record.rt_isr_pin = rt_pin;
+
+    rt_pin_attach_irq(rt_pin, PIN_IRQ_MODE_RISING_FALLING, pulsein_pin_interrupt_cb, RT_NULL);
+    rt_pin_irq_enable(rt_pin, PIN_IRQ_ENABLE); /* enable interrupt */
+    rt_err = rt_sem_take(&pulsein_sem, rt_tick_from_millisecond(timeout/1000));
+    rt_pin_irq_enable(rt_pin, PIN_IRQ_DISABLE); /* disable interrupt */
+
+    if(rt_err == RT_EOK)
+    {
+        delta = (pulse_record.second_pulse_timestamp.sec - pulse_record.first_pulse_timestamp.sec)*1000000;
+        delta += (pulse_record.second_pulse_timestamp.usec - pulse_record.first_pulse_timestamp.usec);
+    }
+    else
+    {
+        delta = 0; /* timeout or other errors */
+    }
+    return delta;
 }
 
 /* Measures the length (in microseconds) of a pulse on the pin; state is HIGH
@@ -38,6 +126,5 @@ unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout)
  */
 unsigned long pulseInLong(uint8_t pin, uint8_t state, unsigned long timeout)
 {
-    LOG_E("pulseInLong() has not been implemented yet!");
-    return 0;
+    return pulseIn(pin, state, timeout);
 }
