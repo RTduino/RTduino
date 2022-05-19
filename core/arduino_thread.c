bbde05a1f7ef9229ac6f1f3837da7b389f0cbcf0
Meco Man <920369182@qq.com>
2022-05-19 11:41:12 -0400

    add hardware timer callback function

--- core/arduino_thread.c
+++ core/arduino_thread.c
@@ -24,6 +24,10 @@
 #define RTDUINO_THREAD_PRIO     30
 #endif /* RTDUINO_THREAD_PRIO */
 
+#ifndef RTDUINO_DEFAULT_HWTIMER_DEVICE_NAME
+#define RTDUINO_DEFAULT_HWTIMER_DEVICE_NAME "timer0" /* dummy name */
+#endif /* RTDUINO_DEFAULT_HWTIMER_DEVICE_NAME */
+
 rt_device_t arduino_hwtimer_device = RT_NULL;
 
 /* initialization for BSP; maybe a blank function  */
@@ -31,9 +35,10 @@ RT_WEAK void initVariant(void)
 {
 }
 
-#ifndef RTDUINO_DEFAULT_HWTIMER_DEVICE_NAME
-#define RTDUINO_DEFAULT_HWTIMER_DEVICE_NAME "timer0" /* dummy name */
-#endif /* RTDUINO_DEFAULT_HWTIMER_DEVICE_NAME */
+static rt_err_t hwtimer_timeout_cb(rt_device_t dev, rt_size_t size)
+{
+    return RT_EOK;
+}
 
 static void hwtimer_init(void)
 {
@@ -52,6 +57,7 @@ static void hwtimer_init(void)
             LOG_E("Failed to open hardware timer!");
             return;
         }
+        rt_device_set_rx_indicate(hwtimer_device, hwtimer_timeout_cb);
         rt_device_control(hwtimer_device, HWTIMER_CTRL_FREQ_SET, &freq);
         rt_device_control(hwtimer_device, HWTIMER_CTRL_MODE_SET, &mode);
         if(rt_device_write(hwtimer_device, 0, &val, sizeof(val)) != 0)
