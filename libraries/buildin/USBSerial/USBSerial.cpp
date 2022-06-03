bc28edb05a52f4b3bab67805a769df73d418d309
Meco Man <920369182@qq.com>
2022-06-03 14:45:57 -0400

    [tinyusb][SerialUSB] add read features

--- libraries/buildin/USBSerial/USBSerial.cpp
+++ libraries/buildin/USBSerial/USBSerial.cpp
@@ -6,44 +6,83 @@
  * Change Logs:
  * Date           Author       Notes
  * 2022-03-24     Meco Man     first version
+ * 2022-06-03     Meco Man     add read features
  */
 
 #include <tusb.h>
+#include <Arduino.h>
 #include "USBSerial.h"
 
 void USBSerial::begin(uint32_t baud)
 {
-    begin(baud, SERIAL_8N1);
+
 }
 
 void USBSerial::begin(uint32_t baud, uint8_t config)
 {
-    /* Nothing need to do */
+
+}
+
+void USBSerial::end(void)
+{
+
 }
 
 /*Code to display letter when given the ASCII code for it*/
-size_t USBSerial::write(uint8_t c)
-{
-    if (tud_cdc_connected())
-    {
-        tud_cdc_write_char(c);
-        tud_cdc_write_flush();
-    }
-    return 1;
-}
+size_t USBSerial::write(uint8_t ch) { return write(&ch, 1); }
 
 /*Code to display array of chars when given a pointer to the beginning of the array and a size
     -- this will not end with the null character*/
 size_t USBSerial::write(const uint8_t *buffer, size_t size)
 {
-    size_t byte_written;
+    size_t remain = size;
 
-    if (tud_cdc_connected())
+    if (remain && tud_cdc_connected())
     {
-        byte_written = tud_cdc_write(buffer, size);
+        size_t wrcount = tud_cdc_write(buffer, size);
+        remain -= wrcount;
+        buffer += wrcount;
         tud_cdc_write_flush();
     }
-    return byte_written;
+    return size - remain;
+}
+
+int USBSerial::available(void)
+{
+    uint32_t count = tud_cdc_available();
+
+    // Add an yield to run usb background in case sketch block wait as follows
+    // while( !Serial.available() ) {}
+    if (!count) {
+      yield();
+    }
+
+    return count;
+}
+
+int USBSerial::peek(void)
+{
+    uint8_t ch;
+    return tud_cdc_peek(&ch) ? (int)ch : -1;
+}
+
+int USBSerial::read(void)
+{
+    return (int)tud_cdc_read_char();
+}
+
+size_t USBSerial::read(uint8_t *buffer, size_t size) {
+  return tud_cdc_read(buffer, size);
+}
+
+int USBSerial::availableForWrite(void)
+{
+    return tud_cdc_write_available();
+}
+
+void USBSerial::flush(void)
+{
+    tud_cdc_write_flush();
 }
 
 USBSerial SerialUSB;
--- libraries/buildin/USBSerial/USBSerial.h
+++ libraries/buildin/USBSerial/USBSerial.h
@@ -11,17 +11,23 @@
 #ifndef USBSerial_h
 #define USBSerial_h
 
-#include <HardwareSerial.h>
+#include <Stream.h>
 
-class USBSerial: public HardwareSerial
+class USBSerial: public Stream
 {
 public:
     void begin(uint32_t baud);
     void begin(uint32_t baud, uint8_t config);
-    // overwrite Print::write
+    void end(void);
+    virtual int available(void);
+    virtual int peek(void);
+    virtual int read(void);
+    size_t read(uint8_t *buffer, size_t size);
+    virtual int availableForWrite(void);
+    virtual void flush(void);
     virtual size_t write(uint8_t c);
     virtual size_t write(const uint8_t *buffer, size_t size);
-    using HardwareSerial::write;
+    using Print::write; // pull in write(str) from Print
 };
 
 extern USBSerial SerialUSB;
