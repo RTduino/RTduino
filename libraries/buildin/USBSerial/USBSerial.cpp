c5157007ba8194b7dd3bb2fb77c081f431c31037
Meco Man <920369182@qq.com>
2022-06-03 23:40:10 -0400

    优化USBSerial TinyUSB与RTduino对接代码

--- libraries/buildin/USBSerial/USBSerial.cpp
+++ libraries/buildin/USBSerial/USBSerial.cpp
@@ -29,7 +29,10 @@ void USBSerial::end(void)
 }
 
 /*Code to display letter when given the ASCII code for it*/
-size_t USBSerial::write(uint8_t ch) { return write(&ch, 1); }
+size_t USBSerial::write(uint8_t ch)
+{
+    return write(&ch, 1);
+}
 
 /*Code to display array of chars when given a pointer to the beginning of the array and a size
     -- this will not end with the null character*/
@@ -37,27 +40,24 @@ size_t USBSerial::write(const uint8_t *buffer, size_t size)
 {
     size_t remain = size;
 
-    if (remain && tud_cdc_connected())
+    while (remain && tud_cdc_connected())
     {
-        size_t wrcount = tud_cdc_write(buffer, size);
+        size_t wrcount = tud_cdc_write(buffer, remain);
         remain -= wrcount;
         buffer += wrcount;
         tud_cdc_write_flush();
+        // Write FIFO is full, run usb background to flush
+        if (remain)
+        {
+            yield();
+        }
     }
     return size - remain;
 }
 
 int USBSerial::available(void)
 {
-    uint32_t count = tud_cdc_available();
-
-    // Add an yield to run usb background in case sketch block wait as follows
-    // while( !Serial.available() ) {}
-    if (!count) {
-      yield();
-    }
-
-    return count;
+    return tud_cdc_available();
 }
 
 int USBSerial::peek(void)
@@ -71,8 +71,9 @@ int USBSerial::read(void)
     return (int)tud_cdc_read_char();
 }
 
-size_t USBSerial::read(uint8_t *buffer, size_t size) {
-  return tud_cdc_read(buffer, size);
+size_t USBSerial::read(uint8_t *buffer, size_t size)
+{
+    return tud_cdc_read(buffer, size);
 }
 
 int USBSerial::availableForWrite(void)
