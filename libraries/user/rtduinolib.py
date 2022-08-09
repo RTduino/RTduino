44224aa62a1788cabbce22692edd034e58170c2b
Meco Man <920369182@qq.com>
2022-08-09 17:09:15 -0400

    [rtduinolib.py] 增加scons --rtduino命令 该命令可以在编译时，通过附加命令来使能一些功能，目前是在编译时弹出一个tkinter窗口

--- libraries/user/rtduinolib.py
+++ libraries/user/rtduinolib.py
@@ -1,7 +1,18 @@
 import Tkinter as tk
+from SCons.Script import AddOption, GetOption
 
-def rtduinolib_import():
+def rtduinolib_gui():
     window = tk.Tk()
     window.wm_attributes('-topmost', 1)
     window.wm_attributes('-topmost', 0)
     window.mainloop()
+
+def rtduinolib_init():
+    AddOption('--rtduino',
+                help='RTduino configuration',
+                action='store_true',
+                default=False,
+                dest='rtduino_configuration')
+
+    if GetOption('rtduino_configuration'):
+        rtduinolib_gui()
