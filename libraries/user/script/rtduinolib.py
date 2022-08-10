60cadcc80761d8d13681ad583cad6c774ec2090d
Meco Man <920369182@qq.com>
2022-08-09 20:57:50 -0400

    将python脚本放置于script文件夹中

--- libraries/user/script/rtduinolib.py
+++ libraries/user/script/rtduinolib.py
@@ -1,18 +1,22 @@
+import os
 import Tkinter as tk
 from SCons.Script import AddOption, GetOption
 
 def rtduinolib_gui():
     window = tk.Tk()
+    window.title('RTduino Settings')
+    window.geometry('327x272')
+    window.iconbitmap('./script/rt-thread-studio.ico')
     window.wm_attributes('-topmost', 1)
     window.wm_attributes('-topmost', 0)
     window.mainloop()
 
 def rtduinolib_init():
     AddOption('--rtduino',
-                help='RTduino configuration',
+                help='RTduino Settings',
                 action='store_true',
                 default=False,
-                dest='rtduino_configuration')
+                dest='rtduino')
 
-    if GetOption('rtduino_configuration'):
+    if GetOption('rtduino'):
         rtduinolib_gui()
