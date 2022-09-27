de13284e11d8b7704bc59cf9b717d7a78e7854e1
Meco Man <920369182@qq.com>
2022-09-27 00:54:43 -0400

    增加arduino focuson模式rt_kprintf

--- core/cmd/arduino_cmd_serial.c
+++ core/cmd/arduino_cmd_serial.c
@@ -15,6 +15,40 @@
 #define DBG_LVL    DBG_INFO
 #include <rtdbg.h>
 
+static rt_bool_t arduino_serial_focuson_mode = RT_FALSE;
+
+int rt_kprintf(const char *fmt, ...)
+{
+    va_list args;
+    rt_size_t length;
+    rt_device_t _console_device;
+    static char rt_log_buf[RT_CONSOLEBUF_SIZE];
+
+    if(arduino_serial_focuson_mode && rt_thread_self() != rt_thread_find("Arduino"))
+    {
+        /* only can be printed in Arduino thread in foucs on mode is enabled */
+        return RT_NULL;
+    }
+
+    va_start(args, fmt);
+    /* the return value of vsnprintf is the number of bytes that would be
+     * written to buffer had if the size of the buffer been sufficiently
+     * large excluding the terminating null byte. If the output string
+     * would be larger than the rt_log_buf, we have to adjust the output
+     * length. */
+    length = rt_vsnprintf(rt_log_buf, sizeof(rt_log_buf) - 1, fmt, args);
+    if (length > RT_CONSOLEBUF_SIZE - 1)
+        length = RT_CONSOLEBUF_SIZE - 1;
+    _console_device = rt_console_get_device();
+    if (_console_device != RT_NULL)
+    {
+        rt_device_write(_console_device, 0, rt_log_buf, length);
+    }
+    va_end(args);
+
+    return length;
+}
+
 struct rt_ringbuffer *console_serial_ringbuffer = RT_NULL;
 
 rt_mutex_t arduino_serial_ringbuffer_mutex;
@@ -28,7 +62,8 @@ void _cmd_serial_foucson(void)
     if(init != RT_TRUE)
     {
         arduino_serial_ringbuffer_mutex =
-                rt_mutex_create("arduino", RT_IPC_FLAG_PRIO);
+                rt_mutex_create("ArdSeri", RT_IPC_FLAG_PRIO);
+        arduino_serial_focuson_mode = RT_TRUE;
         init = RT_TRUE;
     }
 
