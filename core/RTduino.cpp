7fac7a308db651ce558ccda2065f019473d4e585
Meco Man <920369182@qq.com>
2023-05-20 20:36:35 -0400

    add rtduino sketch loader rtduino_sketch_loader_create rtduino_sketch_loader_create_prio rtduino_sketch_loader_delete RTDUINO_SKETCH_LOADER RTDUINO_SKETCH_LOADER_PRIO

--- core/RTduino.cpp
+++ core/RTduino.cpp
@@ -20,45 +20,74 @@
 #define DBG_LVL    DBG_INFO
 #include <rtdbg.h>
 
-#ifndef RTDUINO_NO_SETUP_LOOP
-#ifndef RTDUINO_THREAD_SIZE
-#define RTDUINO_THREAD_SIZE     1024
-#endif /* RTDUINO_THREAD_SIZE */
-
-#ifndef RTDUINO_THREAD_PRIO
-#define RTDUINO_THREAD_PRIO     30
-#endif /* RTDUINO_THREAD_PRIO */
-
-rt_align(RT_ALIGN_SIZE)
-static rt_uint8_t rtduino_thread_stack[RTDUINO_THREAD_SIZE];
-static struct rt_thread rtduino_thread;
-
-static void rtduino_entry(void *parameter)
-{
-    setup();
-    while(1)
-    {
-        loop();
-        rt_schedule();
-    }
-}
-
-#ifdef RTDUINO_USING_SKETCH
-rt_align(RT_ALIGN_SIZE)
-static rt_uint8_t rtduino_sketch_thread_stack[RTDUINO_THREAD_SIZE];
-static struct rt_thread rtduino_sketch_thread;
-
 static void rtduino_sketch_entry(void *parameter)
 {
-    sketch_setup();
+    rtduino_loader_t loader = (rtduino_loader_t)parameter;
+    loader->setup();
     while(1)
     {
-        sketch_loop();
+        loader->loop();
         rt_schedule();
     }
 }
-#endif /* RTDUINO_USING_SKETCH */
-#endif /* RTDUINO_NO_SETUP_LOOP */
+
+rtduino_loader_t rtduino_sketch_loader_create_prio(const char* name, void (*setup)(void), void (*loop)(void), rt_uint32_t stack_size, rt_uint8_t priority)
+{
+    rtduino_loader_t loader;
+
+    if (name == RT_NULL || setup == RT_NULL || loop == RT_NULL)
+    {
+        return RT_NULL;
+    }
+
+    loader = (rtduino_loader_t)rt_malloc(sizeof(rtduino_loader));
+    if (loader == RT_NULL)
+    {
+        return RT_NULL;
+    }
+
+    loader->name =  name;
+    loader->setup = setup;
+    loader->loop = loop;
+    loader->thread = RT_NULL;
+
+    rt_thread_t thread = rt_thread_create(name, rtduino_sketch_entry, (void *)loader, stack_size, priority, 20);
+    if (thread != RT_NULL)
+    {
+        loader->thread = thread;
+        rt_thread_startup(thread);
+    }
+    else
+    {
+        rt_free(loader);
+        loader = RT_NULL;
+    }
+
+    return loader;
+}
+
+rtduino_loader_t rtduino_sketch_loader_create(const char* name, void (*setup)(void), void (*loop)(void))
+{
+    return rtduino_sketch_loader_create_prio(name, setup, loop, RTDUINO_THREAD_SIZE, RTDUINO_THREAD_PRIO);
+}
+
+rt_err_t rtduino_sketch_loader_delete(rtduino_loader_t loader)
+{
+    rt_err_t err;
+
+    if(loader == RT_NULL)
+    {
+        return -RT_EINVAL;
+    }
+
+    err = rt_thread_delete(loader->thread);
+    if(err == RT_EOK)
+    {
+        rt_free(loader);
+    }
+
+    return err;
+}
 
 #ifdef RTDUINO_DEFAULT_HWTIMER_DEVICE_NAME
 static void hwtimer_init(void)
@@ -103,36 +132,7 @@ static int rtduino_init(void)
     initVariant();
 
 #ifndef RTDUINO_NO_SETUP_LOOP
-    rt_err_t rt_err = rt_thread_init(&rtduino_thread, "RTduino",
-                            rtduino_entry, RT_NULL,
-                            rtduino_thread_stack,
-                            RTDUINO_THREAD_SIZE,
-                            RTDUINO_THREAD_PRIO, 20);
-
-    if (rt_err == RT_EOK)
-    {
-        rt_thread_startup(&rtduino_thread);
-    }
-    else
-    {
-        LOG_E("RTduino thread initialization failed!");
-    }
-#ifdef RTDUINO_USING_SKETCH
-    rt_err = rt_thread_init(&rtduino_sketch_thread, "sketch",
-                            rtduino_sketch_entry, RT_NULL,
-                            rtduino_sketch_thread_stack,
-                            RTDUINO_THREAD_SIZE,
-                            RTDUINO_THREAD_PRIO, 20);
-
-    if (rt_err == RT_EOK)
-    {
-        rt_thread_startup(&rtduino_sketch_thread);
-    }
-    else
-    {
-        LOG_E("RTduino sketch thread initialization failed!");
-    }
-#endif /* RTDUINO_USING_SKETCH */
+    rtduino_sketch_loader_create("RTduino", setup, loop);
 #endif /* RTDUINO_NO_SETUP_LOOP */
     return 0;
 }
--- core/RTduino.h
+++ core/RTduino.h
@@ -21,14 +21,40 @@
 #define rt_weak      RT_WEAK
 #endif
 
+#ifndef RTDUINO_THREAD_SIZE
+#define RTDUINO_THREAD_SIZE     2048
+#endif /* RTDUINO_THREAD_SIZE */
+
+#ifndef RTDUINO_THREAD_PRIO
+#define RTDUINO_THREAD_PRIO     30
+#endif /* RTDUINO_THREAD_PRIO */
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#ifdef RTDUINO_USING_SKETCH
-void sketch_setup(void);
-void sketch_loop(void);
-#endif /* RTDUINO_USING_SKETCH */
+typedef struct
+{
+    const char* name;
+    void (*setup)(void);
+    void (*loop)(void);
+    rt_thread_t thread;
+}rtduino_loader, *rtduino_loader_t;
+
+rtduino_loader_t rtduino_sketch_loader_create(const char* name, void (*setup)(void), void (*loop)(void));
+rtduino_loader_t rtduino_sketch_loader_create_prio(const char* name, void (*setup)(void), void (*loop)(void), rt_uint32_t stack_size, rt_uint8_t priority);
+rt_err_t rtduino_sketch_loader_delete(rtduino_loader_t loader);
+
+#define RTDUINO_SKETCH_LOADER_PRIO(name, setup, loop, stack_size, priority) \
+    static int sketch_loader(void) \
+    { \
+        rtduino_sketch_loader_create_prio(name, setup, loop, stack_size, priority);\
+        return 0; \
+    } \
+    INIT_APP_EXPORT(sketch_loader)
+
+#define RTDUINO_SKETCH_LOADER(name, setup, loop) \
+    RTDUINO_SKETCH_LOADER_PRIO(name, setup, loop, RTDUINO_THREAD_SIZE, RTDUINO_THREAD_PRIO)
 
 #ifdef __cplusplus
 } // extern "C"
