570f2e5f71758b1b70a2617703a78dc6ffc7c794
Meco Man <920369182@qq.com>
2024-02-24 15:28:37 -0500

    fix the license

--- core/RTduino.h
+++ core/RTduino.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2021-2022, RTduino Development Team
  *
- * SPDX-License-Identifier: LGPL-v2.1
+ * SPDX-License-Identifier: Apache-2.0
  *
  * https://github.com/RTduino/RTduino
  * https://gitee.com/rtduino/RTduino
