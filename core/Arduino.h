c73c7b12651cb5ec3e44a024091cd07a5cd49b6c
Meco Man <920369182@qq.com>
2024-04-07 10:49:00 -0400

    remove switchToPWM

--- core/Arduino.h
+++ core/Arduino.h
@@ -729,14 +729,6 @@ void loop(void);
  */
 void initVariant(void);
 
-/**
- * @ingroup System
- * @brief Switches to the PWM device.
- * @param bus_name The name of the PWM device to switch to.
- * @note Do not call this function manually.
- */
-void switchToPWM(const char *dev_name);
-
 #ifdef RTDUINO_USING_SPI
 /**
  * @ingroup System
